b3e647ef7b793597f373a1457a630479f45e09ef
Radosław Rzepka <radoslawrzepka02@gmail.com>
2025-03-14 20:02:12 +0100

    fix: fix vercel deploy, add 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
@@ -9,12 +9,8 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://radoslawrzepka.me",
   integrations: [sitemap(), react()],
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: "static",
+  adapter: vercel({ webAnalytics: { enabled: true } }),
   vite: {
     plugins: [tailwindcss()],
   },
